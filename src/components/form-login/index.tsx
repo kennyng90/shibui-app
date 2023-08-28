79c35ce6b484e8679754c01409d727e7ae4da5bb
kennyng90 <kennamngu@gmail.com>
2023-08-28 20:05:52 +0200

    Added supabase authentication

--- src/components/form-login/index.tsx
+++ src/components/form-login/index.tsx
@@ -1,82 +1,43 @@
-const FormLogin = () => {
+import { FormEvent, useState } from 'react';
+import { supabase } from '@/lib/auth/client';
+
+export default function Auth() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithOtp({ email });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      alert('Check your email for the login link!');
+    }
+    setLoading(false);
+  };
+
   return (
-    <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 lg:py-0">
-      <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
-        <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-          <h1 className="text-xl font-bold text-gray-900 md:text-2xl">
-            Sign in to your account
-          </h1>
-          <form className="space-y-4 md:space-y-6" action="#">
-            <div>
-              <label
-                htmlFor="email"
-                className="mb-2 block text-sm font-medium text-gray-900"
-              >
-                Your email
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-gray-600 focus:ring-gray-600 sm:text-sm"
-                placeholder="name@company.com"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="mb-2 block text-sm font-medium text-gray-900"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-gray-600 focus:ring-gray-600 sm:text-sm"
-              />
-            </div>
-            <div className="flex items-center justify-between">
-              <div className="flex items-start">
-                <div className="flex h-5 items-center">
-                  <input
-                    id="remember"
-                    aria-describedby="remember"
-                    type="checkbox"
-                    className="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800"
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label htmlFor="remember" className="text-gray-500">
-                    Remember me
-                  </label>
-                </div>
-              </div>
-              <a
-                href="#"
-                className="text-sm font-medium text-gray-600 hover:underline"
-              >
-                Forgot password?
-              </a>
-            </div>
-            <button
-              type="submit"
-              className="w-full rounded-lg bg-gray-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-            >
-              Sign in
-            </button>
-            <p className="text-sm font-light text-gray-500">
-              Don’t have an account yet?{' '}
-              <a href="#" className="font-medium text-gray-600 hover:underline">
-                Sign up
-              </a>
-            </p>
-          </form>
-        </div>
-      </div>
+    <div className="mt-28 flex border-black">
+      <form className="grid w-full gap-10" onSubmit={handleLogin}>
+        <input
+          className="w-full border-b-2 border-black py-2 text-5xl focus:outline-none"
+          type="email"
+          autoFocus
+          placeholder="your@email.com"
+          value={email}
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button
+          className="w-fit rounded bg-black px-3 py-2 text-left text-white hover:bg-gray-800"
+          disabled={loading}
+        >
+          {loading ? <span>Loading</span> : <span>Send magic link</span>}
+        </button>
+      </form>
     </div>
   );
-};
-
-export default FormLogin;
+}
